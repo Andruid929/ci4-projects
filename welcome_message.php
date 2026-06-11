dc0222b9a21ac95f7081e7be7b854d3cf5772a36
Andruid929 <princeajsa@gmail.com>
2026-06-11 11:31:47 +0200

    Initial commit

--- welcome_message.php
+++ welcome_message.php
@@ -300,6 +300,9 @@
         <p>Page rendered in {elapsed_time} seconds using {memory_usage} MB of memory.</p>
 
         <p>Environment: <?= ENVIRONMENT ?></p>
+
+    </div>
+
     <div class="copyrights">
 
         <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
