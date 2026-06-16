5236b8507bbdc9fd8dfc5a9679c5c3abd9f328cc
Andruid929 <princeajsa@gmail.com>
2026-06-16 09:35:16 +0200

    Added SB Admin 2 elements

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -9,10 +9,10 @@ use App\Helpers\PageNameHelper;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= PageNameHelper::getPageName() ?></title>
-    <link href="<?= site_url('vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= site_url('js-vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="<?= site_url('css/sb-admin-2.min.css') ?>" rel="stylesheet">
-    <link href="<?= site_url('vendor/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
+    <link href="<?= site_url('js-vendor/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
     <style>
         /* DataTables sort icon overrides */
         table.dataTable > thead .sorting:before,
@@ -112,11 +112,11 @@ use App\Helpers\PageNameHelper;
     <!-- Response Modal (for AJAX messages) -->
     <?= view('components/modals/response') ?>
     
-    <script src="<?= site_url('vendor/jquery/jquery.min.js') ?>"></script>
-    <script src="<?= site_url('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-    <script src="<?= site_url('vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
-    <script src="<?= site_url('vendor/datatables/jquery.dataTables.min.js') ?>"></script>
-    <script src="<?= site_url('vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script>
+    <script src="<?= site_url('js-vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= site_url('js-vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= site_url('js-vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+    <script src="<?= site_url('js-vendor/datatables/jquery.dataTables.min.js') ?>"></script>
+    <script src="<?= site_url('js-vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script>
     <script src="<?= site_url('js/sb-admin-2.min.js') ?>"></script>
     <script src="<?= site_url('js/datatable-init.js') ?>"></script>
     <script src="<?= site_url('js/summary-init.js') ?>"></script>
